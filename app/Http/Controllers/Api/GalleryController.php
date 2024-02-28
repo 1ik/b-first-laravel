fdd4e64ae706c9265caeda662ca17c772ce7bc44
nazmul <hossainnazmul191@gmail.com>
2024-02-28 18:00:53 +0600

    old data insert script logic somehting change

--- app/Http/Controllers/Api/GalleryController.php
+++ app/Http/Controllers/Api/GalleryController.php
@@ -21,28 +21,34 @@ class GalleryController extends Controller
     }
 
     public function oldDataUpload(ImageUploadService $imageUploadService){
-        $old_category = OldCategory::all();
         $posts        = OldStory::all();
 
         DB::transaction(function() use($posts,$imageUploadService) {
-            foreach($posts as $post){
+            foreach ($posts as $post) {
                 $story = new Story();
-                $story->title =  $post->title;
+                $story->title = $post->title;
                 $story->content = $post->content;
-                $story->meta = ['headline'=>$post->brief,'featured_image'=>$post->featuredPhotoUrl,'featured_image_caption'=>$post->feature_image_caption];
+                $story->meta = [
+                    'headline' => $post->brief,
+                    'featured_image' => $post->featuredPhotoUrl,
+                    'featured_image_caption' => $post->feature_image_caption
+                ];
                 $story->created_at = $post->created_at;
                 $story->updated_at = $post->updated_at;
                 $story->save();
-                $story->categories()->attach($post->category_id);
+            
+                if (!empty($post->category_id)) {
+                    $get_cat = OldCategory::find($post->category_id);
+                    $category = Category::firstOrCreate(['name' => $get_cat->name]);
+                    $story->categories()->attach($category->id);
+                }
+            
                 if (!empty($post->author)) {
                     $author = Author::firstOrCreate(['name' => $post->author]);
-                    if ($author->wasRecentlyCreated) {
-                        $story->authors()->attach($author->id);
-                    } else {
-                        $story->authors()->attach($author->id);
-                    }
+                    $story->authors()->attach($author->id);
                 }
-            }   
+            }
+            
         },5);
 
     }
