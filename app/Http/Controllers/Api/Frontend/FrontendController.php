a9f7c3e67b77225290a705d04c8a5df56cf89e42
Nazmul Hossain <hossainnazmul191@gmail.com>
2024-02-29 15:18:49 +0600

    pagination logic in public api

--- app/Http/Controllers/Api/Frontend/FrontendController.php
+++ app/Http/Controllers/Api/Frontend/FrontendController.php
@@ -15,20 +15,21 @@ class FrontendController extends Controller
 
         return CategoryResource::collection(Category::all());
     }
-    public function latestStories(){
-
-        return StoryResource::collection(Story::with(['authors', 'categories', 'tags'])->latest()->paginate(20));
+    public function latestStories(Request $request){
+        $pageSize = $request->input('size', 20);
+        return StoryResource::collection(Story::with(['authors', 'categories', 'tags'])->latest()->paginate($pageSize));
     }
 
-    public function categoryStories($category){
+    public function categoryStories(Request $request,$category){
 
+        $pageSize = $request->input('size', 20);
         $category = Category::where('name', $category)->first();
 
         if (!$category) {
             return response()->json(['error' => 'Category not found'], 404);
         }
 
-        $stories = $category->stories()->with('authors', 'tags','categories')->paginate(20); 
+        $stories = $category->stories()->with('authors', 'tags','categories')->paginate($pageSize); 
 
         return StoryResource::collection($stories);
     }
