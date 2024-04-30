3532d11f62deba9003c02f43528b623e3497a576
mahbubalamsiddik <mahbubalamsiddik97@gmail.com>
2024-04-30 14:36:23 +0600

    Added missing item for category color code

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,6 +18,7 @@ class CategoryResource extends JsonResource
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'deleted_at' => $this->deleted_at,
+            'color_code' => $this->color_code
         ];
     }
 }
